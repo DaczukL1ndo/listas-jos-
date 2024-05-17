0273a42bfb70a5482f00a661ceb0c6afb0103a5f
Matheus Daczuk <matheus.daczuk.rodrigues@escola.pr.gov.br>
2024-05-17 12:29:43 -0300

    Exercicio 6

---
+++ exercicio6.js
@@ -0,0 +1,20 @@
+const numero1 = 4;
+
+
+const numerofloat = 3.7;
+
+
+const numeroint = 43;
+
+console.log("impressão 1 uma linha só");
+console.log(numero1 + " " + numerofloat + " " + numeroint)
+
+
+console.log("impressão com TAB")
+console.log(numero + "      " + numerofloat + "     " + numeroint)
+
+console.log("impressão em outra linha")
+console.log(numero1 + " " + numerofloat + " " + numeroint)
+
+
+
