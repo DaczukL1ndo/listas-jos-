bb70ed2b4309c8d4b460f4f6839cfdc8d9886c6e
DaczukL1ndo <141372262+DaczukL1ndo@users.noreply.github.com>
2024-06-28 09:22:55 -0300

    Add files via upload

---
+++ lista2/exercicio1lista2.js
@@ -0,0 +1,8 @@
+let lado1 = 2
+let lado2 = 2
+let lado3 = 5
+let lado4 = 5
+
+console.log(lado1 * lado3)
+console.log(lado1 + lado2 + lado3 + lado4)
+
---
+++ lista2/exercicio2lista2.js
@@ -0,0 +1,7 @@
+let lado1 = 4
+let lado2 = 4
+let lado3 = 4
+let lado4 = 4
+
+console.log(lado1 + lado2 + lado3 + lado4)
+console.log(lado1 * lado2 )
